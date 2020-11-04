39b9771243dc85d74d6df4f6d86ade95a069f3b7
bugrahan <umutbugrahanozkan@gmail.com>
2020-11-04 19:02:17 +0300

    add file Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String ... args) {
+	createGUI();
+    
+    }
+
+    public static void createGUI() {
+	JFrame frame = new JFrame("Test!");
+	frame.setSize(400, 400);
+	frame.setVisible(true);
+    }
+}
