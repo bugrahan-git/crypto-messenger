d8260b50ebf59d3fd16bbc9be457c4439957cbbe
bugrahan <umutbugrahanozkan@gmail.com>
2020-11-13 22:27:41 +0300

    fix buttons

--- src/Client.java
+++ src/Client.java
@@ -7,15 +7,18 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.JRadioButton;
 import javax.swing.JTextPane;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Client extends JFrame {
-
-	private JPanel contentPane;
+	
+	
+	public Client() {
+		initializeGUI();
+	}
 
 	/**
 	 * Launch the application.
@@ -32,57 +35,68 @@ public class Client extends JFrame {
 			}
 		});
 	}
-
+	
+	private JPanel contentPane;
+	
+	private JButton btnConnect;
+	private JButton btnDisconnect;
+	
+	private JRadioButton rdbtnAes;
+	private JRadioButton rdbtnDes;
+	private JRadioButton rdbtnCbc;
+	private JRadioButton rdbtnOfb;
+	
+	private JTextPane textPaneChat;				// Main chat text-pane
+	private JTextPane textPaneText;				// Message to send text-pane
+	private JTextPane textPaneCryptedtext;		// Crypted message text-pane
+	
+	private JButton btnEncrypt;
+	private JButton btnSend;
+	
+	private JLabel isConnected;
+	
 	/**
 	 * Create the frame.
 	 */
-	public Client() {
+	
+	private void initializeGUI() {
 		setTitle("Crypto Messenger");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 800);
+		
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JLabel lblNewLabel = new JLabel("Server");
-		
+		JLabel lblServer = new JLabel("Server");
 		JLabel lblMethod = new JLabel("Method");
-		
 		JLabel lblMode = new JLabel("Mode");
 		
-		JButton btnConnect = new JButton("\u25B6 Connect");
+		btnConnect = new JButton("\u25B6 Connect");
+		btnDisconnect = new JButton("\u25FE Disconnect");
 		
-		JButton btnDisconnect = new JButton("\u25FE Disconnect");
-		btnDisconnect.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
+		rdbtnAes = new JRadioButton("AES");
+		rdbtnDes = new JRadioButton("DES");
+		rdbtnCbc = new JRadioButton("CBC");
+		rdbtnOfb = new JRadioButton("OFB");
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("AES");
-		
-		JRadioButton rdbtnDes = new JRadioButton("DES");
-		
-		JRadioButton rdbtnCbc = new JRadioButton("CBC");
-		
-		JRadioButton rdbtnOfb = new JRadioButton("OFB");
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setEditable(false);
+		textPaneChat = new JTextPane();
 		
 		JLabel lblText = new JLabel("Text");
-		
 		JLabel lblCryptedText = new JLabel("Crypted Text");
 		
-		JButton btnEncrypt = new JButton("Encrypt");
+		btnEncrypt = new JButton("Encrypt");
+		btnSend = new JButton("Send");
 		
-		JButton btnSend = new JButton("Send");
+		textPaneText = new JTextPane();
+		textPaneCryptedtext = new JTextPane();
 		
-		JTextPane textPane_text = new JTextPane();
+		textPaneCryptedtext.setEditable(false);
+		textPaneChat.setEditable(false);
+		textPaneText.setEditable(false);
 		
-		JTextPane textPane_cryptedtext = new JTextPane();
-		textPane_cryptedtext.setEditable(false);
-		
-		JLabel isConnected = new JLabel("Not Connected");
+		isConnected = new JLabel("Not Connected");
 		isConnected.setFont(new Font("Dialog", Font.ITALIC, 13));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
@@ -91,7 +105,7 @@ public class Client extends JFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(lblNewLabel)
+							.addComponent(lblServer)
 							.addGap(184)
 							.addComponent(lblMethod))
 						.addGroup(gl_contentPane.createSequentialGroup()
@@ -99,7 +113,7 @@ public class Client extends JFrame {
 							.addPreferredGap(ComponentPlacement.UNRELATED)
 							.addComponent(btnDisconnect)
 							.addPreferredGap(ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
-							.addComponent(rdbtnNewRadioButton)))
+							.addComponent(rdbtnAes)))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(rdbtnDes)
 					.addGap(18)
@@ -115,12 +129,12 @@ public class Client extends JFrame {
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addContainerGap()
 							.addComponent(lblText))
-						.addComponent(textPane_text, GroupLayout.PREFERRED_SIZE, 183, GroupLayout.PREFERRED_SIZE))
+						.addComponent(textPaneText, GroupLayout.PREFERRED_SIZE, 183, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textPane_cryptedtext, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE)
+							.addComponent(textPaneCryptedtext, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addComponent(btnSend, GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)
@@ -128,7 +142,7 @@ public class Client extends JFrame {
 						.addComponent(lblCryptedText))
 					.addContainerGap())
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 490, GroupLayout.PREFERRED_SIZE)
+					.addComponent(textPaneChat, GroupLayout.PREFERRED_SIZE, 490, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addComponent(isConnected, GroupLayout.PREFERRED_SIZE, 463, GroupLayout.PREFERRED_SIZE)
@@ -141,14 +155,14 @@ public class Client extends JFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 							.addComponent(lblMethod)
-							.addComponent(lblNewLabel))
+							.addComponent(lblServer))
 						.addComponent(lblMode))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(1)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(rdbtnNewRadioButton)
+								.addComponent(rdbtnAes)
 								.addComponent(rdbtnDes)
 								.addComponent(rdbtnCbc)
 								.addComponent(rdbtnOfb)))
@@ -156,23 +170,52 @@ public class Client extends JFrame {
 							.addComponent(btnConnect)
 							.addComponent(btnDisconnect)))
 					.addGap(18)
-					.addComponent(textPane, GroupLayout.DEFAULT_SIZE, 534, Short.MAX_VALUE)
+					.addComponent(textPaneChat, GroupLayout.DEFAULT_SIZE, 534, Short.MAX_VALUE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addComponent(lblCryptedText)
 						.addComponent(lblText))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(textPane_text, GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE)
+						.addComponent(textPaneText, GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addComponent(btnEncrypt)
 							.addGap(3)
 							.addComponent(btnSend, GroupLayout.DEFAULT_SIZE, 64, Short.MAX_VALUE))
-						.addComponent(textPane_cryptedtext, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE))
+						.addComponent(textPaneCryptedtext, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(isConnected))
 		);
 		contentPane.setLayout(gl_contentPane);
+		
+		btnDisconnect.setEnabled(false);
+		btnEncrypt.setEnabled(false);
+		btnSend.setEnabled(false);
+		
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnConnect.setEnabled(false);
+				btnDisconnect.setEnabled(true);
+				textPaneText.setEditable(true);
+				btnEncrypt.setEnabled(true);
+				btnSend.setEnabled(true);
+				isConnected.setText("Connected");
+				
+			}
+		});
+		
+		btnDisconnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnConnect.setEnabled(true);
+				btnDisconnect.setEnabled(false);
+				textPaneText.setEditable(false);
+				btnEncrypt.setEnabled(false);
+				btnSend.setEnabled(false);
+				isConnected.setText("Disconnected");
+			}
+		});
+		
 	}
+		
 }
 
