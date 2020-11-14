11e86625280faca0c18fbc6e4fc3b57efceb9139
bugrahan <umutbugrahanozkan@gmail.com>
2020-11-14 20:03:07 +0300

    add start() method

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,25 @@
+import java.net.ServerSocket;
 
 public class Server {
-        
-
+    private int port;
+    
+    public Server() {
+	this.port = 32222;
+    }
+    
+    public static void main(String ... args) {
+	Server S = new Server();
+	
+	S.start();
+    }    
+    
+    private void start() {
+	try(ServerSocket ss = new ServerSocket(this.port)) {
+	    System.out.println("Server is running on port " + this.port); 
+	    while(true);
+	    
+	}catch(Exception e) {
+	    e.printStackTrace();
+	}
+    }
 }
