124ebae3df5f73e7cee14496ee1a51246ff3a23d
bugrahan <umutbugrahanozkan@gmail.com>
2020-11-15 17:32:56 +0300

    add inputstream

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,11 @@
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     private int port;
-    
+    private Socket socket = null;
+    private DataInputStream in = null;
     public Server() {
 	this.port = 32222;
     }
@@ -14,10 +17,14 @@ public class Server {
     }    
     
     private void start() {
-	try(ServerSocket ss = new ServerSocket(this.port)) {
-	    System.out.println("Server is running on port " + this.port); 
-	    while(true);
-	    
+		System.out.println("Server is running on port " + this.port);
+		try(ServerSocket ss = new ServerSocket(this.port)) {
+			while (true){
+				socket = ss.accept();
+				in = new DataInputStream(
+					new BufferedInputStream(socket.getInputStream()));
+				System.out.println(in.readUTF());
+			}
 	}catch(Exception e) {
 	    e.printStackTrace();
 	}
